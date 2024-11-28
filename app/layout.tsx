49465c96c2fd0b179687b31c34eda4c237c6adbc
Johannes Hildén <johannes@knodd.se>
2024-11-28 09:23:58 +0100

    Clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import "./styles/globals.css";
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 export const metadata = {
-  title: 'Quartr',
-  description: 'Financial Research and Investor Relations Platform',
+  title: "Quartr",
+  description: "Financial Research and Investor Relations Platform",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head />
