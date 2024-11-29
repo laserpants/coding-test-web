5aa0bed659e5ca01b14a10e6ba8128f06fd4f8a8
Johannes Hildén <johannes@knodd.se>
2024-11-29 16:53:52 +0100

    Small fixes

--- app/api/companies/route.ts
+++ app/api/companies/route.ts
@@ -1,20 +1,21 @@
 import { NextResponse } from "next/server";
 import mockCompanies from "../../data/companies";
 
-/*
+/**
  * GET /api/companies
  */
-export async function GET(req: Request) {
-  const url = new URL(req.url);
+export async function GET(request: Request) {
+  const url = new URL(request.url);
   const search = url.searchParams.get("search");
 
   // Filter companies if a "search" query parameter is provided
   const filteredCompanies = search
-    ? mockCompanies.filter((company) =>
-        company.companyName.toLowerCase().includes(search.toLowerCase()),
+    ? mockCompanies.filter(({ companyName }) =>
+        companyName.toLowerCase().includes(search.toLowerCase()),
       )
     : mockCompanies;
 
+  // Paginate the response
   const page = parseInt(url.searchParams.get("page") || "1", 10);
   const limit = parseInt(url.searchParams.get("limit") || "10", 10);
 
