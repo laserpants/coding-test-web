73eef49faab6d82036bf4328a219dcb4c02722b8
Johannes Hildén <johannes@knodd.se>
2024-11-29 15:25:57 +0100

    Minor change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import { Inter } from "next/font/google";
 import "./styles/globals.css";
+import { Inter } from "next/font/google";
 import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
