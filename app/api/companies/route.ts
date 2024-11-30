016005f702e41461a66d955878408951c0eb72eb
Johannes Hildén <johannes@knodd.se>
2024-11-30 18:21:13 +0100

    Install winston and add basic logging

--- app/api/companies/route.ts
+++ app/api/companies/route.ts
@@ -1,33 +1,50 @@
 import { NextResponse } from "next/server";
 import mockCompanies from "../../data/companies";
+import logger from "../../utils/logger";
 
 /**
  * GET /api/companies
+ *
+ * TODO: This is a placeholder implementation. Replace this with real API.
  */
 export async function GET(request: Request) {
-  const url = new URL(request.url);
-  const search = url.searchParams.get("search");
+  logger.info("GET /api/companies called");
 
-  // Filter companies if a "search" query parameter is provided
-  const filteredCompanies = search
-    ? mockCompanies.filter(({ companyName }) =>
-        companyName.toLowerCase().includes(search.toLowerCase()),
-      )
-    : mockCompanies;
+  try {
+    const url = new URL(request.url);
+    const search = url.searchParams.get("search");
 
-  // Paginate the response
-  const page = parseInt(url.searchParams.get("page") || "1", 10);
-  const limit = parseInt(url.searchParams.get("limit") || "10", 10);
+    // Filter companies if a "search" query parameter is provided
+    const filteredCompanies = search
+      ? mockCompanies.filter(({ companyName }) =>
+          companyName.toLowerCase().includes(search.toLowerCase()),
+        )
+      : mockCompanies;
 
-  const paginatedCompanies = filteredCompanies.slice(
-    (page - 1) * limit,
-    page * limit,
-  );
+    // Paginate the response
+    const page = parseInt(url.searchParams.get("page") || "1", 10);
+    const limit = parseInt(url.searchParams.get("limit") || "10", 10);
 
-  return NextResponse.json({
-    data: paginatedCompanies,
-    total: filteredCompanies.length,
-    page,
-    limit,
-  });
+    const paginatedCompanies = filteredCompanies.slice(
+      (page - 1) * limit,
+      page * limit,
+    );
+
+    logger.info("Successfully fetched companies", {
+      total: filteredCompanies.length,
+      page,
+      limit,
+    });
+
+    return NextResponse.json({
+      data: paginatedCompanies,
+      total: filteredCompanies.length,
+      page,
+      limit,
+    });
+  } catch (error) {
+    logger.error("Failed to fetch companies", { error });
+
+    return NextResponse.error();
+  }
 }
