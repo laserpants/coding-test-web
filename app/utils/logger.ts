b8f935c3a8b2ab8097d9b225523911e5b66c7acb
Johannes Hildén <johannes@knodd.se>
2024-12-01 12:27:59 +0100

    Disable logging during tests

--- app/utils/logger.ts
+++ app/utils/logger.ts
@@ -23,7 +23,9 @@ const logger = createLogger({
     format.json(), // Logs in JSON format
   ),
   transports: [
-    new transports.Console(),
+    new transports.Console({
+      silent: process.env.NODE_ENV === "test",
+    }),
     new transports.File({ filename: "logs/error.log", level: "error" }),
     new transports.File({ filename: "logs/combined.log" }),
   ],
